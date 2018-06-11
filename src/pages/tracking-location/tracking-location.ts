c881936573a2c77194889986f3a406507ee4c3a4
SivaRamAditya <sivaramadityatummala@gmail.com>
2018-06-12 00:30:43 +0530

    Update Map

--- src/pages/tracking-location/tracking-location.ts
+++ src/pages/tracking-location/tracking-location.ts
@@ -1,8 +1,9 @@
-import { Component, ViewChild, ElementRef, OnInit, NgZone } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { IonicPage, Platform } from 'ionic-angular';
 import { Geolocation, GeolocationOptions, Geoposition } from '@ionic-native/geolocation';
 import { BackgroundGeolocation } from '@ionic-native/background-geolocation';
 import { Diagnostic } from '@ionic-native/diagnostic';
+import { filter } from 'rxjs/operators';
 
 declare var google;
 
@@ -12,18 +13,20 @@ declare var google;
     templateUrl: 'tracking-location.html'
 })
 export class TrackingLocationPage implements OnInit {
+    currentMapTrack: any;
     geoOptions: GeolocationOptions;
     currentPosition: Geoposition;
     geolocation: any;
     @ViewChild('map') mapElement: ElementRef;
     @ViewChild('dest') destElement: ElementRef;
     map: any;
+    trackedRoute = [];
     marker: any;
     watchCounter: number = 0;
     autocomplete: any;
     directionsDisplay = new google.maps.DirectionsRenderer;
     origin: string;
-    constructor(private geoLocation: Geolocation, private zone: NgZone, private platform: Platform, private diagnostic: Diagnostic, private backgroundLocation: BackgroundGeolocation) { }
+    constructor(private geoLocation: Geolocation, private platform: Platform, private diagnostic: Diagnostic, private backgroundLocation: BackgroundGeolocation) { }
 
     ngOnInit(): void {
         if (this.platform.navigatorPlatform() === "Win32") {
@@ -31,73 +34,69 @@ export class TrackingLocationPage implements OnInit {
             this.autocomplete = new google.maps.places.Autocomplete(this.destElement.nativeElement);
             this.getUserPosition();
         } else {
-            this.platform.ready().then(()=> {
-             this.diagnostic.isLocationEnabled().then((isAvailable) => {
-                alert("Your current location is defaultly selected as starting point");
-                this.autocomplete = new google.maps.places.Autocomplete(this.destElement.nativeElement);
-                this.getUserPosition();
-             }).catch((error) => {
-                alert(JSON.stringify(error));
-                alert('Please enable the location details');
-             });
-            }).catch((error)=> {
-              console.log(error);
-            });            
+            this.platform.ready().then(() => {
+                this.diagnostic.isLocationEnabled().then((isAvailable) => {
+                    alert("Your current location is defaultly selected as starting point");
+                    this.autocomplete = new google.maps.places.Autocomplete(this.destElement.nativeElement);
+                    this.getUserPosition();
+                }).catch((error) => {
+                    alert(JSON.stringify(error));
+                    alert('Please enable the location details');
+                });
+            }).catch((error) => {
+                console.log(error);
+            });
             this.autocomplete = new google.maps.places.Autocomplete(this.destElement.nativeElement);
             this.getUserPosition();
         }
     }
 
     getUserPosition() {
-        const options = {
-            enableHighAccuracy: false,
-            frequency: 3000
-        };
-        // this.geoLocation.getCurrentPosition(options).then((pos: Geoposition) => {
-        //     console.log(pos);
-        //     // if(!this.trackingTime)
-        //     //  this.updateWatchPosition();
-        //     this.zone.run(() => {
-        //         this.getUserPosition();
-        //         alert("Your Latitude is "+pos.coords.latitude);
-        //     });
-        //     this.addMap(pos.coords.latitude,pos.coords.longitude);
-        // }, (err: PositionError) => {
-        //     console.log("error : " + err.message);
-        // });
-        this.geoLocation.watchPosition(options).subscribe((position: Geoposition) => {
-            if (this.currentPosition !== position && this.watchCounter == 0) {
-                this.geolocation = '';
-                this.currentPosition = position;
-                this.addMap(position.coords.latitude, position.coords.longitude);
-                this.watchCounter = 1;
-            } else {
-                //this.addMap(position.coords.latitude, position.coords.longitude);
-               if (this.currentPosition.coords.latitude !== position.coords.latitude && this.currentPosition.coords.longitude !== position.coords.longitude) {
-                let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                this.updateGeocode(latLng);
-                this.map.setCenter(latLng);
-              }
-            }
-        }, (err: PositionError) => {
-            console.log("error : " + err.message);
-        });
-    }
-
-    addMap(lat, long) {
-
-        let latLng = new google.maps.LatLng(lat, long);
-
         let mapOptions = {
-            center: latLng,
             zoom: 15,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
 
         this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-        this.updateGeocode(latLng);
-        //this.addMarker();
+        this.geoLocation.getCurrentPosition().then((position: Geoposition) => {
+            console.log(position);
+            const latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            this.map.setCenter(latLng);
+            this.trackedRoute.push({ lat: +position.coords.latitude, lng: +position.coords.longitude});
+            //this.updateGeocode(latLng);
+        }, (err: PositionError) => {
+            console.log("error : " + err.message);
+        });
 
+    }
+
+    startTracking() {
+
+        const options = {
+            enableHighAccuracy: false,
+            frequency: 3000
+        };
+
+        this.trackedRoute = [];
+        this.geoLocation.watchPosition(options).pipe(
+            filter((p) => p.coords !== undefined) //Filter Out Errors
+        ).subscribe((position: Geoposition) => {
+            if (this.currentPosition !== position) { //&& this.watchCounter == 0) {
+                this.geolocation = '';
+                this.currentPosition = position;
+                this.filterDuplicates(position.coords.latitude, position.coords.longitude);
+                this.redrawPath(this.trackedRoute);
+                this.watchCounter = 1;
+            } else {
+                // if (this.currentPosition.coords.latitude !== position.coords.latitude && this.currentPosition.coords.longitude !== position.coords.longitude) {
+                //     let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                //     this.updateGeocode(latLng);
+                //     this.map.setCenter(latLng);
+                // }
+            }
+        }, (err: PositionError) => {
+            console.log("error : " + err.message);
+        });
     }
 
     addMarker(latLng: any) {
@@ -119,6 +118,23 @@ export class TrackingLocationPage implements OnInit {
         });
     }
 
+    redrawPath(path) {
+        if (this.currentMapTrack) {
+          this.currentMapTrack.setMap(null);
+        }
+     
+        if (path.length > 1) {
+          this.currentMapTrack = new google.maps.Polyline({
+            path: path,
+            geodesic: true,
+            strokeColor: '#ff00ff',
+            strokeOpacity: 1.0,
+            strokeWeight: 3
+          });
+          this.currentMapTrack.setMap(this.map);
+        }
+      }
+
     updateGeocode(latLng) {
         let geocoder = new google.maps.Geocoder();
         geocoder.geocode({ 'location': latLng }, (results, status) => {
@@ -129,8 +145,6 @@ export class TrackingLocationPage implements OnInit {
                     if (this.autocomplete.getPlace() && this.autocomplete.getPlace().formatted_address) {
                         this.getDirections();
                     }
-                    //this.map.setCenter(results[0].geometry.location);
-                    //this.addMarker();
                 }
             } else {
                 alert('Geocode was not successful for the following reason: ' + status);
@@ -138,30 +152,38 @@ export class TrackingLocationPage implements OnInit {
         });
     }
 
+    filterDuplicates(lat, lng) {
+        for(let index = 0; index< this.trackedRoute.length; index++) {
+            if(this.trackedRoute[index].lat !== lat && this.trackedRoute[index].lng !== lng ) {
+                this.trackedRoute.push({ lat: +lat, lng: +lng});
+                break;
+            }
+        }
+    }
     getDirections() {
         const dest = this.autocomplete.getPlace().formatted_address;
-        const directionsService = new google.maps.DirectionsService;
+        //const directionsService = new google.maps.DirectionsService;
+        const location = this.autocomplete.getPlace().geometry.location;
+        this.filterDuplicates(location.lat(), location.lng());
+        
+        if( this.trackedRoute.length >= 2) {
+            this.redrawPath(this.trackedRoute);
+            this.addMarker(new google.maps.LatLng(location.lat(), location.lng()));
+           // this.startTracking();
+        }
+        
+        // this.directionsDisplay.setMap(this.map);
 
-        this.directionsDisplay.setMap(this.map);
-
-        directionsService.route({
-            origin: this.origin,
-            destination: dest,
-            travelMode: 'DRIVING'
-        }, (response, status) => {
-            if (status === 'OK') {
-                this.directionsDisplay.setDirections(response);
-            } else {
-                alert('Directions request failed due to ' + status);
-            }
-        });
+        // directionsService.route({
+        //     origin: this.origin,
+        //     destination: dest,
+        //     travelMode: 'DRIVING'
+        // }, (response, status) => {
+        //     if (status === 'OK') {
+        //         this.directionsDisplay.setDirections(response);
+        //     } else {
+        //         alert('Directions request failed due to ' + status);
+        //     }
+        // });
     }
-    // updateWatchPosition() {
-    //    if(this.trackingTime){
-    //        clearTimeout(this.trackingTime);
-    //    }
-    //    this.trackingTime = setTimeout(() => {
-    //        this.getUserPosition();
-    //    }, 1000);
-    // }
 }
